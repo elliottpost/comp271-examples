586b6b8de0952954f1a9997a8206d3ca0842a562
Elly Post <epost@luc.edu>
2020-03-12 19:14:15 -0500

    Add string example for linearsearch

--- src/unit1/LinearSearch.java
+++ src/unit1/LinearSearch.java
@@ -12,17 +12,21 @@ public class LinearSearch<E> {
 
     public static void main(String[] args) {
         Integer[] numbers = {5, 23, 8, 1, 6, 12, 9};
-        LinearSearch<Integer> ls = new LinearSearch<Integer>(numbers);
+//        LinearSearch<Integer> ls = new LinearSearch<Integer>(numbers);
+
+        String[] strings = {"Hello", "World", "welcome to", "comp271"};
+        LinearSearch<String> ls = new LinearSearch<>(strings);
 
         Scanner sc = new Scanner(System.in);
         while(true) {
             System.out.print("Enter a number to search the array: ");
-            int searchTerm = sc.nextInt();
+//            int searchTerm = sc.nextInt();
+            String searchTerm = sc.next();
             int idx = ls.indexOf(searchTerm);
             if (idx >= 0) {
-                System.out.println("".format("Found %d at position %d", searchTerm, idx));
+                System.out.println("".format("Found %s at position %d", searchTerm, idx));
             } else {
-                System.out.println("".format("%d does not exist in the array", searchTerm));
+                System.out.println("".format("%s does not exist in the array", searchTerm));
             }
         }
     }
