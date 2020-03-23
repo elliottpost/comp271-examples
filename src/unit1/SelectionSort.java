bf387ab97dfa5926f1aad136a4b5bc94557e0731
Elly Post <elly@luc.edu>
2020-03-23 18:07:39 -0500

    Add selection sort updates from thurs class

---
+++ src/unit1/SelectionSort.java
@@ -0,0 +1,31 @@
+package unit1;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 5, 2, 1, 8, 6};
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIdx = i;
+
+            for(int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j; // i = 1; j = 2
+                }
+            }
+
+            if (minIdx != i) {
+                int temp = arr[minIdx];
+                arr[minIdx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        for(int x = 0; x < n; x++) {
+            System.out.print(arr[x] + " ");
+        }
+
+        System.out.println("\nQED");
+    }
+}
